f856a76df8c250f101af960e8dff60b025820e10
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:09:37 +0330

    update layout using Sidebar comp

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import Sidebar from './partials/Sidebar.vue';
 
 </script>
 
 <template>
-  <div>
-    <RouterView />
+  <div class="h-screen w-screen grid grid-cols-3 bg-[#f3f3f3] dark:bg-[#202020]">
+    <div class="col-span-1">
+      <Sidebar />
+    </div>
+
+    <div class="col-span-2">
+      <RouterView />
+    </div>
   </div>
 </template>
