9b60aad9c5aa5f6c2ccaa3742b02b93e3565deff
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 21:28:58 +0330

    update styles

--- src/layouts/partials/Sidebar.vue
+++ src/layouts/partials/Sidebar.vue
@@ -6,7 +6,7 @@ import SettingsButton from '../../components/sidebar/buttons/SettingsButton.vue'
 </script>
 
 <template>
-  <div class="size-full bg-inherit p-5 space-y-1">
+  <div class="h-full w-1/3 fixed top-0 start-0 bg-inherit p-5 space-y-1">
     <SettingsButton />
     <BreakMessagesButton />
     <AdvancedButton />
