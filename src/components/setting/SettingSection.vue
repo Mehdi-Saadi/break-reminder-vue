3acbad4fc1c0d68fa080bbb679ea0bd5e311958a
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 19:06:55 +0330

    update font style

--- src/components/setting/SettingSection.vue
+++ src/components/setting/SettingSection.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="relative pt-2">
-    <div class="border border-gray-300 rounded-md p-4">
+    <div class="border border-gray-300 rounded-md p-4 text-sm">
       <span class="absolute top-0 start-2 text-xs font-semibold bg-[#f3f3f3] dark:bg-[#202020] px-1">{{ label }}</span>
 
       <slot />
