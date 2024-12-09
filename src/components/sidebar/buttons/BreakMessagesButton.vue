10e766fce8d85b33664007e1b1992cf33b6d53db
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:49:54 +0330

    add BreakMessagesButton

---
+++ src/components/sidebar/buttons/BreakMessagesButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from './Button.vue';
+import ChatBubbleIcon from '../../icons/ChatBubbleIcon.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <Button
+    :active="router.currentRoute.value.name === 'break.messages'"
+    :to="{ name: 'break.messages' }"
+  >
+    <ChatBubbleIcon class="size-5 me-1.5" />
+    <span>Break Messages</span>
+  </Button>
+</template>
