d3719db9767fffac48f46d0c76dd67d7b1437eac
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 19:00:16 +0330

    add AddButton

---
+++ src/components/setting/buttons/AddButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AddIcon from '../../icons/AddIcon.vue';
+import SquareButton from './SquareButton.vue';
+</script>
+
+<template>
+  <SquareButton>
+    <AddIcon class="size-4" />
+  </SquareButton>
+</template>
