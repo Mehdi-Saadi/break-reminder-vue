5761caeb92261a9d81949bd61a251ab9532a66e4
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 19:04:37 +0330

    add RemoveButton

---
+++ src/components/setting/buttons/RemoveButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import RemoveIcon from '../../icons/RemoveIcon.vue';
+import SquareButton from './SquareButton.vue';
+</script>
+
+<template>
+  <SquareButton>
+    <RemoveIcon class="size-4" />
+  </SquareButton>
+</template>
