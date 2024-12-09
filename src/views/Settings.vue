0264702248e1204abdcdd3c66e90239fb1c2df6a
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 20:15:21 +0330

    create second setting using SettingItem

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,20 +1,41 @@
 <script setup lang="ts">
 import NumberField from '../components/setting/NumberField.vue';
+import SettingItem from '../components/setting/SettingItem.vue';
 import SettingSection from '../components/setting/SettingSection.vue';
 </script>
 
 <template>
   <div class="flex flex-col">
     <SettingSection label="Short Breaks">
-      <div class="flex items-center justify-between">
-        <span>Interval between two breaks (in minutes)</span>
-        <NumberField
-          :default-value="20"
-          :max="120"
-          :step="5"
-          @update="(newVal) => console.log(newVal)"
-        />
-      </div>
+      <SettingItem>
+        <template #text>
+          Interval between two breaks (in minutes)
+        </template>
+
+        <template #button>
+          <NumberField
+            :default-value="20"
+            :max="120"
+            :step="5"
+            @update="(newVal) => console.log(newVal)"
+          />
+        </template>
+      </SettingItem>
+
+      <SettingItem>
+        <template #text>
+          Break duration (in seconds)
+        </template>
+
+        <template #button>
+          <NumberField
+            :default-value="20"
+            :max="120"
+            :step="5"
+            @update="(newVal) => console.log(newVal)"
+          />
+        </template>
+      </SettingItem>
     </SettingSection>
   </div>
 </template>
