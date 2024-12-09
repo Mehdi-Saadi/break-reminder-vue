ff5d4b7d415fd2f4eeb7764fa7a501604295e814
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 13:40:08 +0330

    configure router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div
-    class="font-bold text-red-800"
-    id="myId"
-  >
-    Hello world!
-  </div>
+  <RouterView />
 </template>
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Welcome from '../views/Welcome.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: Welcome,
+    }
+  ],
+  strict: true,
+  sensitive: true,
+});
+
+export default router;
