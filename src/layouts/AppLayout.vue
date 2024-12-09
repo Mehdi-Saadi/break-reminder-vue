96aae73450488c50d2b093d2f9d8b7500ba135a3
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 18:35:47 +0330

    select none

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -4,7 +4,7 @@ import Sidebar from './partials/Sidebar.vue';
 </script>
 
 <template>
-  <div class="h-screen w-screen grid grid-cols-3 bg-[#f3f3f3] text-gray-800 dark:text-white dark:bg-[#202020]">
+  <div class="h-screen w-screen grid grid-cols-3 bg-[#f3f3f3] text-gray-800 dark:bg-[#202020] dark:text-white select-none">
     <div class="col-span-1">
       <Sidebar />
     </div>
