cdb442b8d40c67ed67181e9f33658293a45f1c92
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:50:23 +0330

    use BreakMessages button

--- src/layouts/partials/Sidebar.vue
+++ src/layouts/partials/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import BreakMessagesButton from '../../components/sidebar/buttons/BreakMessagesButton.vue';
 import SettingsButton from '../../components/sidebar/buttons/SettingsButton.vue';
 
 </script>
@@ -6,5 +7,6 @@ import SettingsButton from '../../components/sidebar/buttons/SettingsButton.vue'
 <template>
   <div class="h-screen w-full bg-inherit p-5 space-y-1">
     <SettingsButton />
+    <BreakMessagesButton />
   </div>
 </template>
