d5e6a60071011f9a1068d562464007d19bf8b97f
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 20:13:31 +0330

    add SettingItem

---
+++ src/components/setting/SettingItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-between">
+    <span class="pb-0.5">
+      <slot name="text" />
+    </span>
+
+    <slot name="button" />
+  </div>
+</template>
