fe4ec3c8ca4e2f46a1144c3689785de7a04e5c2c
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 18:36:12 +0330

    add SettingSection

---
+++ src/components/SettingSection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="relative pt-2">
+    <div class="border border-gray-300 rounded-md p-4">
+      <span class="absolute top-0 start-2 text-xs font-semibold bg-[#f3f3f3] dark:bg-[#202020] px-1">{{ label }}</span>
+
+      <slot />
+    </div>
+  </div>
+</template>
