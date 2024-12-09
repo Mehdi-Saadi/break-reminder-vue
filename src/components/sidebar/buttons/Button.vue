e36ce86d54b14cc0c4c1b2f5d8e7b0b287bdb5c6
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:06:51 +0330

    add base sidebar button

---
+++ src/components/sidebar/buttons/Button.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouteLocationRaw } from 'vue-router';
+
+defineProps<{
+  active: boolean
+  to: RouteLocationRaw
+}>();
+</script>
+
+<template>
+  <RouterLink
+    :to="to"
+    :class="[active ? 'bg-[#eaeaea] hover:bg-[#ededed]' : 'hover:bg-[#eaeaea]']"
+    class="flex items-center pe-4 py-2 rounded-md text-sm font-semibold"
+  >
+    <span
+      v-show="active"
+      class="min-w-0.5 min-h-4 max-w-0.5 max-h-4 bg-[#0067c0] rounded-full"
+    />
+
+    <div
+      :class="[active ? 'ps-1' : 'ps-1.5']"
+      class="flex items-center"
+    >
+      <slot />
+    </div>
+  </RouterLink>
+</template>
