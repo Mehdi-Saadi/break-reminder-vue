196dcb1d8663a12dd34407598b9bcc92733ee47c
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 19:08:02 +0330

    add NumberField style

---
+++ src/components/setting/NumberField.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AddButton from './buttons/AddButton.vue';
+import RemoveButton from './buttons/RemoveButton.vue';
+</script>
+
+<template>
+  <div class="flex items-center border border-gray-300 rounded-md overflow-hidden">
+    <input
+      class="h-6 w-9 focus:outline-0 px-1 bg-inherit border-e border-gray-300"
+      type="text"
+    />
+    <RemoveButton class="border-e border-gray-300" />
+    <AddButton />
+  </div>
+</template>
