d5f9147fa53412a5be37c969a8f47b1d95c4b0d9
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:08:41 +0330

    add sidebar

---
+++ src/layouts/partials/Sidebar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import SettingsButton from '../../components/sidebar/buttons/SettingsButton.vue';
+
+</script>
+
+<template>
+  <div class="h-screen w-full bg-inherit p-5 space-y-1">
+    <SettingsButton />
+  </div>
+</template>
