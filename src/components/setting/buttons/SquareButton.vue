44bc4c61b7e3f26311380bfd7ebef7441ec33e25
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 18:59:54 +0330

    add SquareButton

---
+++ src/components/setting/buttons/SquareButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="flex items-center justify-center size-6 active:bg-[#eaeaea] hover:bg-[#ededed] active:dark:bg-[#2d2d2d] hover:dark:bg-[#292929]"
+    type="button"
+  >
+    <slot />
+  </button>
+</template>
