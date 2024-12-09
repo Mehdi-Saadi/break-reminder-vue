2edefe05e1b6dfd9bd353f1e8e0ce2145308b59e
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 20:02:41 +0330

    add first setting

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-
+import NumberField from '../components/setting/NumberField.vue';
+import SettingSection from '../components/setting/SettingSection.vue';
 </script>
 
 <template>
-  <div class="font-bold text-red-800">Settings!</div>
+  <div class="flex flex-col">
+    <SettingSection label="Short Breaks">
+      <div class="flex items-center justify-between">
+        <span>Interval between two breaks (in minutes)</span>
+        <NumberField
+          :default-value="20"
+          :max="120"
+          :step="5"
+          @update="(newVal) => console.log(newVal)"
+        />
+      </div>
+    </SettingSection>
+  </div>
 </template>
