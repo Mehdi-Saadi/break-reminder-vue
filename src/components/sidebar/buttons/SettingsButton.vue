ac070ccf747ef4a882aa1c0ab4085fbe174ff678
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 17:07:42 +0330

    add sidebar settings button

---
+++ src/components/sidebar/buttons/SettingsButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import GearIcon from '../../icons/GearIcon.vue';
+import Button from './Button.vue';
+
+const router = useRouter();
+</script>
+
+<template>
+  <Button
+    :active="router.currentRoute.value.name === 'settings'"
+    :to="{ name: 'settings' }"
+  >
+    <GearIcon class="size-5 me-1.5" />
+    <span>Settings</span>
+  </Button>
+</template>
