7e599ff3b6dca17bff9e797c93be9839cdda7f10
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 18:04:08 +0330

    add AdvancedButton

---
+++ src/components/sidebar/buttons/AdvancedButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from './Button.vue';
+import DoubleArrowRightIcon from '../../icons/DoubleArrowRightIcon.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <Button
+    :active="router.currentRoute.value.name === 'advanced'"
+    :to="{ name: 'advanced' }"
+  >
+    <DoubleArrowRightIcon class="size-5 me-1.5" />
+    <span>Advanced</span>
+  </Button>
+</template>
