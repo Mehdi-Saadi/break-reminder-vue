fd8fc3693f154e456bdf5daa353b2d407f4ce08f
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-09 20:55:54 +0330

    add CheckboxButton

---
+++ src/components/setting/buttons/CheckboxButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const value = defineModel<boolean>({ required: true });
+</script>
+
+<template>
+  <input
+    v-model="value"
+    class="size-4 cursor-pointer"
+    type="checkbox"
+  />
+</template>
