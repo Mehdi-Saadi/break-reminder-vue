749f3e8837e07dfa5dab8946bb7a4b09367bffbc
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 10:32:55 +0330

    add SettingButton

---
+++ src/components/ui/buttons/SettingButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import GearIcon from '@/components/ui/icons/GearIcon.vue';
+import SquareButton from '@/components/ui/buttons/SquareButton.vue';
+</script>
+
+<template>
+  <SquareButton class="rounded-full border border-gray-300">
+    <GearIcon class="size-4" />
+  </SquareButton>
+</template>
