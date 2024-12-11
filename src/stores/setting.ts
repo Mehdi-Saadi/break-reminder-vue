288e3b2e48ee74be4b1babf39177b24c99b98fa6
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 18:29:11 +0330

    add break messages state

--- src/stores/setting.ts
+++ src/stores/setting.ts
@@ -1,4 +1,5 @@
-import { IAdvancedSettings, IBreakSettings, ISettingOptions } from '@/types/settings';
+import { IAdvancedSettings, BreakMessages, IBreakSettings, ISettingOptions } from '@/types/settings';
+import { loadMessages, saveMessages } from '@/utils/message.ts';
 import { loadSettings, saveSettings } from '@/utils/setting';
 import { defineStore } from 'pinia';
 import { ref, watch } from 'vue';
@@ -44,6 +45,35 @@ const saveSettingOptions = (settings: ISettingOptions): void =>
   saveSettings('options', settings);
 
 
+// break messages
+const defaultShortBreakMessages: Readonly<BreakMessages> = Object.freeze({
+  '88027327-d0a0-42c3-a990-363e865d49e3': 'Tightly close your eyes',
+  'db36bc4b-6d4f-42d2-a10f-f31f8fd8fe8d': 'Roll your eyes a few times to each side',
+  '60981702-125f-411b-ac70-98fc6b7a5c8c': 'Rotate your eyes in clockwise direction',
+  'a1416a32-977d-4035-832b-30245bb9444b': 'Rotate your eyes in counterclockwise direction',
+  '6d18d092-2f49-4e16-99b0-f95538f6c479': 'Blink your eyes',
+  '141dfbfc-6f24-4fac-84ab-0835b0f11584': 'Focus on a point in the far distance',
+  '707748c5-4629-47f8-9ed7-e94db77632e9': 'Have some water',
+});
+
+const loadShortBreakMessages = (): BreakMessages =>
+  loadMessages('shortBreakMessages', defaultShortBreakMessages);
+
+const saveShortBreakMessages = (messages: BreakMessages): void =>
+  saveMessages('shortBreakMessages', messages);
+
+const defaultLongBreakMessages: Readonly<BreakMessages> = Object.freeze({
+  'b077fa44-9219-4bfc-a665-2e3a87a95727': 'Walk for a while',
+  'aa88fa9a-51a3-40a7-b8a3-35a7bdf247d0': 'Lean back at your seat and relax',
+});
+
+const loadLongBreakMessages = (): BreakMessages =>
+  loadMessages('longBreakMessages', defaultLongBreakMessages);
+
+const saveLongBreakMessages = (messages: BreakMessages): void =>
+  saveMessages('longBreakMessages', messages);
+
+
 // advanced
 const defaultAdvancedSettings: Readonly<IAdvancedSettings> = Object.freeze({
   doNotDisturb: true,
@@ -66,6 +96,8 @@ const useSettingStore = defineStore('setting', () => {
   const shortBreak = ref<IBreakSettings>(loadShortBreakSettings());
   const longBreak = ref<IBreakSettings>(loadLongBreakSettings());
   const options = ref<ISettingOptions>(loadSettingOptions());
+  const shortBreakMessages = ref<BreakMessages>(loadShortBreakMessages());
+  const longBreakMessages = ref<BreakMessages>(loadLongBreakMessages());
   const advanced = ref<IAdvancedSettings>(loadAdvancedSettings());
 
 
@@ -73,6 +105,8 @@ const useSettingStore = defineStore('setting', () => {
     shortBreak.value = {...defaultShortBreakSettings};
     longBreak.value = {...defaultLongBreakSettings};
     options.value = {...defaultSettingOptions};
+    shortBreakMessages.value = {...defaultShortBreakMessages};
+    longBreakMessages.value = {...defaultLongBreakMessages};
     advanced.value = {...defaultAdvancedSettings};
   };
 
@@ -80,12 +114,16 @@ const useSettingStore = defineStore('setting', () => {
   watch(shortBreak, () => saveShortBreakSettings(shortBreak.value));
   watch(longBreak, () => saveLongBreakSettings(longBreak.value));
   watch(options, () => saveSettingOptions(options.value));
+  watch(shortBreakMessages, () => saveShortBreakMessages(shortBreakMessages.value));
+  watch(longBreakMessages, () => saveLongBreakMessages(longBreakMessages.value));
   watch(advanced, () => saveAdvancedSettings(advanced.value));
 
   return {
     shortBreak,
     longBreak,
     options,
+    shortBreakMessages,
+    longBreakMessages,
     advanced,
     resetSettingsToDefault,
   };
