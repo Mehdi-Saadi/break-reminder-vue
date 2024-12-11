d55acd63e6bac78de459a646f0973fc55fca5fde
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 12:24:16 +0330

    add DeleteButton

---
+++ src/components/ui/buttons/DeleteButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DeleteIcon from '@/components/ui/icons/DeleteIcon.vue';
+import SquareButton from '@/components/ui/buttons/SquareButton.vue';
+</script>
+
+<template>
+  <SquareButton class="rounded-full border border-gray-300 text-red-800">
+    <DeleteIcon class="size-4" />
+  </SquareButton>
+</template>
