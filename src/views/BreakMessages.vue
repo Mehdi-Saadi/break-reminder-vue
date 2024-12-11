9a140bb06f5bd31929812ac1404baf61f5d58450
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 19:30:14 +0330

    use store

--- src/views/BreakMessages.vue
+++ src/views/BreakMessages.vue
@@ -2,32 +2,45 @@
 import DeleteButton from '@/components/ui/buttons/DeleteButton.vue';
 import SettingItem from '@/components/setting/SettingItem.vue';
 import SettingSection from '@/components/setting/SettingSection.vue';
+import useSettingStore from '@/stores/setting.ts';
+import { UUID } from '@/types/general';
+
+const useSetting = useSettingStore();
+const { deleteBreakMessageById } = useSetting;
 </script>
 
 <template>
   <div class="flex flex-col space-y-4">
     <!-- short breaks -->
     <SettingSection label="Short Breaks">
-      <SettingItem>
+      <SettingItem
+        v-for="(value, id) in useSetting.shortBreakMessages"
+        :key="id"
+        class="pb-3"
+      >
         <template #text>
-          Tightly close your eyes
+          {{ value }}
         </template>
 
         <template #button>
-          <DeleteButton />
+          <DeleteButton @click="deleteBreakMessageById(id as UUID)" />
         </template>
       </SettingItem>
     </SettingSection>
 
     <!-- long breaks -->
     <SettingSection label="Long Breaks">
-      <SettingItem>
+      <SettingItem
+        v-for="(value, id) in useSetting.longBreakMessages"
+        :key="id"
+        class="pb-3"
+      >
         <template #text>
-          Walk for a while
+          {{ value }}
         </template>
 
         <template #button>
-          <DeleteButton />
+          <DeleteButton @click="deleteBreakMessageById(id as UUID)" />
         </template>
       </SettingItem>
     </SettingSection>
