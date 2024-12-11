ca3a1b4074f40af021dcf9feffb0c27ad7952acc
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 13:04:20 +0330

    update setting styles

--- src/views/Advanced.vue
+++ src/views/Advanced.vue
@@ -2,6 +2,8 @@
 import CheckboxButton from '@/components/ui/buttons/CheckboxButton.vue';
 import DoNotDisturbOnIcon from '@/components/ui/icons/DoNotDisturbOnIcon.vue';
 import SettingItem from '@/components/setting/SettingItem.vue';
+import MusicNoteIcon from '@/components/ui/icons/MusicNoteIcon.vue';
+import NotificationsIcon from '@/components/ui/icons/NotificationsIcon.vue';
 </script>
 
 <template>
@@ -26,7 +28,16 @@ import SettingItem from '@/components/setting/SettingItem.vue';
     </SettingItem>
     <SettingItem>
       <template #text>
-        Notification
+        <div class="flex items-center space-x-1">
+          <!-- icon -->
+          <NotificationsIcon class="size-5" />
+
+          <!-- text -->
+          <div class="flex flex-col">
+            <div class="font-semibold">Notification</div>
+            <div class="text-xs text-[#5c5c5c] dark:text-[#cccccc]">Show a system notification before breaks</div>
+          </div>
+        </div>
       </template>
 
       <template #button>
@@ -35,7 +46,16 @@ import SettingItem from '@/components/setting/SettingItem.vue';
     </SettingItem>
     <SettingItem>
       <template #text>
-        Audible Alert
+        <div class="flex items-center space-x-1">
+          <!-- icon -->
+          <MusicNoteIcon class="size-5" />
+
+          <!-- text -->
+          <div class="flex flex-col">
+            <div class="font-semibold">Audible Alert</div>
+            <div class="text-xs text-[#5c5c5c] dark:text-[#cccccc]">Play an audible alert at the end of breaks</div>
+          </div>
+        </div>
       </template>
 
       <template #button>
