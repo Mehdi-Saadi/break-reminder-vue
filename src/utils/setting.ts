cbee8c71aeebdf9be23348faecf14915d99d5afd
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 17:40:34 +0330

    use StorageKey type, move store specific utils to store

--- src/utils/setting.ts
+++ src/utils/setting.ts
@@ -1,13 +1,6 @@
-import { IAdvancedSettings, IBreakSettings, ISettingOptions } from '@/types/settings';
+type StorageKey = 'shortBreak' | 'longBreak' | 'options' | 'advanced';
 
-const storageKeys = Object.freeze({
-  shortBreak: 'shortBreak',
-  longBreak: 'longBreak',
-  options: 'options',
-  advanced: 'advanced',
-});
-
-const loadSettings = <T extends object>(storageKey: string, defaultSettings: Readonly<T>): T => {
+export const loadSettings = <T extends object>(storageKey: StorageKey, defaultSettings: Readonly<T>): T => {
   const localData = localStorage.getItem(storageKey);
 
   if (!localData) {
@@ -29,64 +22,5 @@ const loadSettings = <T extends object>(storageKey: string, defaultSettings: Rea
   }
 };
 
-const saveSettings = <T extends object>(storageKey: string, settings: T): void =>
+export const saveSettings = <T extends object>(storageKey: StorageKey, settings: T): void =>
   localStorage.setItem(storageKey, JSON.stringify(settings));
-
-
-// short break
-const defaultShortBreakSettings: Readonly<IBreakSettings> = Object.freeze({
-  intervalBetweenTwoBreaks: 20,
-  breakDuration: 20,
-});
-
-export const loadShortBreakSettings = (): IBreakSettings =>
-  loadSettings(storageKeys.shortBreak, defaultShortBreakSettings);
-
-export const saveShortBreakSettings = (settings: IBreakSettings): void =>
-  saveSettings(storageKeys.shortBreak, settings);
-
-
-// long break
-const defaultLongBreakSettings: Readonly<IBreakSettings> = Object.freeze({
-  intervalBetweenTwoBreaks: defaultShortBreakSettings.intervalBetweenTwoBreaks * 3,
-  breakDuration: defaultShortBreakSettings.breakDuration * 6,
-});
-
-export const loadLongBreakSettings = (): IBreakSettings =>
-  loadSettings(storageKeys.longBreak, defaultLongBreakSettings);
-
-export const saveLongBreakSettings = (settings: IBreakSettings): void =>
-  saveSettings(storageKeys.longBreak, settings);
-
-
-// options
-const defaultSettingOptions: Readonly<ISettingOptions> = Object.freeze({
-  timeToPrepareForBreak: 10,
-  strictBreak: false,
-  allowPostponingBreaks: false,
-  postponeDuration: 5,
-});
-
-export const loadSettingOptions = (): ISettingOptions =>
-  loadSettings(storageKeys.options, defaultSettingOptions);
-
-export const saveSettingOptions = (settings: ISettingOptions): void =>
-  saveSettings(storageKeys.options, settings);
-
-
-// advanced
-const defaultAdvancedSettings: Readonly<IAdvancedSettings> = Object.freeze({
-  doNotDisturb: true,
-  notification: true,
-  audibleAlert: true,
-  selectedAudio: '',
-  smartPause: true,
-  screensaver: true,
-  darkMode: false,
-});
-
-export const loadAdvancedSettings = (): IAdvancedSettings =>
-  loadSettings(storageKeys.advanced, defaultAdvancedSettings);
-
-export const saveAdvancedSettings = (settings: IAdvancedSettings): void =>
-  saveSettings(storageKeys.advanced, settings);
