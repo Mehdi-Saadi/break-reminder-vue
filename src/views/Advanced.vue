69c9a7c5e47d9eee620bcfdc76f0652dc91c3cad
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 12:39:43 +0330

    add base Advanced view

--- src/views/Advanced.vue
+++ src/views/Advanced.vue
@@ -1,7 +1,54 @@
 <script setup lang="ts">
-
+import CheckboxButton from '@/components/ui/buttons/CheckboxButton.vue';
+import SettingItem from '@/components/setting/SettingItem.vue';
 </script>
 
 <template>
-  <div class="font-bold text-red-800">Advanced!</div>
+  <div class="flex flex-col space-y-3 p-4 text-sm">
+    <SettingItem>
+      <template #text>
+        Do Not Disturb
+      </template>
+
+      <template #button>
+        <CheckboxButton :model-value="true" />
+      </template>
+    </SettingItem>
+    <SettingItem>
+      <template #text>
+        Notification
+      </template>
+
+      <template #button>
+        <CheckboxButton :model-value="true" />
+      </template>
+    </SettingItem>
+    <SettingItem>
+      <template #text>
+        Audible Alert
+      </template>
+
+      <template #button>
+        <CheckboxButton :model-value="true" />
+      </template>
+    </SettingItem>
+    <SettingItem>
+      <template #text>
+        Smart Pause
+      </template>
+
+      <template #button>
+        <CheckboxButton :model-value="true" />
+      </template>
+    </SettingItem>
+    <SettingItem>
+      <template #text>
+        Screen saver
+      </template>
+
+      <template #button>
+        <CheckboxButton :model-value="true" />
+      </template>
+    </SettingItem>
+  </div>
 </template>
