b69d4d62f786a60e414ef015a950acf61bfa7fff
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 12:26:14 +0330

    add base BreakMessages view

--- src/views/BreakMessages.vue
+++ src/views/BreakMessages.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
-
+import DeleteButton from '@/components/ui/buttons/DeleteButton.vue';
+import SettingItem from '@/components/setting/SettingItem.vue';
+import SettingSection from '@/components/setting/SettingSection.vue';
 </script>
 
 <template>
-  <div class="font-bold text-red-800">Break Messages!</div>
+  <div class="flex flex-col space-y-4">
+    <!-- short breaks -->
+    <SettingSection label="Short Breaks">
+      <SettingItem>
+        <template #text>
+          Tightly close your eyes
+        </template>
+
+        <template #button>
+          <DeleteButton />
+        </template>
+      </SettingItem>
+    </SettingSection>
+
+    <!-- long breaks -->
+    <SettingSection label="Long Breaks">
+      <SettingItem>
+        <template #text>
+          Walk for a while
+        </template>
+
+        <template #button>
+          <DeleteButton />
+        </template>
+      </SettingItem>
+    </SettingSection>
+  </div>
 </template>
