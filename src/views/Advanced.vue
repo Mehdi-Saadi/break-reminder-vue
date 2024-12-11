79ba4b449b0f274faa83bb75701506ec24a907a1
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-11 12:52:54 +0330

    update setting item

--- src/views/Advanced.vue
+++ src/views/Advanced.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CheckboxButton from '@/components/ui/buttons/CheckboxButton.vue';
+import DoNotDisturbOnIcon from '@/components/ui/icons/DoNotDisturbOnIcon.vue';
 import SettingItem from '@/components/setting/SettingItem.vue';
 </script>
 
@@ -7,7 +8,16 @@ import SettingItem from '@/components/setting/SettingItem.vue';
   <div class="flex flex-col space-y-3 p-4 text-sm">
     <SettingItem>
       <template #text>
-        Do Not Disturb
+        <div class="flex items-center space-x-1">
+          <!-- icon -->
+          <DoNotDisturbOnIcon class="size-5" />
+
+          <!-- text -->
+          <div class="flex flex-col">
+            <div class="font-semibold">Do Not Disturb</div>
+            <div class="text-xs text-[#5c5c5c] dark:text-[#cccccc]">Skip the break if the active window is in fullscreen mode</div>
+          </div>
+        </div>
       </template>
 
       <template #button>
