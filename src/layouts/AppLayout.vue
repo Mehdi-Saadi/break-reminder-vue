f91578012275e2587074efaf2aa42535bd98ab71
Mehdi <mehdi.0.saadi@gmail.com>
2024-12-23 21:02:17 +0330

    toggle dark mode based on settings

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import Sidebar from '@/layouts/partials/Sidebar.vue';
+import useSettingStore from '@/stores/setting.ts';
+
+const useSetting = useSettingStore();
 </script>
 
 <template>
-  <div class="size-full min-h-screen grid grid-cols-3 bg-[#f3f3f3] text-gray-800 dark:bg-[#202020] dark:text-white select-none">
+  <div
+    :class="{ 'dark': useSetting.advanced.darkMode }"
+    class="size-full min-h-screen grid grid-cols-3 bg-[#f3f3f3] text-gray-800 dark:bg-[#202020] dark:text-white select-none"
+  >
     <div class="col-span-1">
       <Sidebar />
     </div>
